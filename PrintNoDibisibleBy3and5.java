3a150af720ac018b0b84119c84635b57a1adac45
Pensionthieng Hrangchal <87723912+PesoHrangchal@users.noreply.github.com>
2022-09-29 15:13:31 +0530

    Update PrintNoDibisibleBy3and5.java

--- PrintNoDibisibleBy3and5.java
+++ PrintNoDibisibleBy3and5.java
@@ -1,10 +1,25 @@
 //Hands-on Exercise 2
 public class PrintNoDibisibleBy3and5 {
 	public static void main(String args[]) {
+		System.out.println("Divisible by 3:");
 		for(int i=1;i<=100;i++) {
-			if(i%3==0&&i%5==0) {
+			if(i%3==0)
+				
 				System.out.print(i+" ");
 			}
+		System.out.println();
+		
+		System.out.println("Divisible by 5:");
+		for(int i=1;i<=100;i++) {
+				if(i%5==0)
+					System.out.print(i+" ");
+		}
+		System.out.println();
+		
+		System.out.println("Divisible by both 3 and 5:");
+		for(int i=1;i<=100;i++) {
+			if(i%3==0&&i%5==0)
+				System.out.print(i+" ");
 		}
 	}
 }
