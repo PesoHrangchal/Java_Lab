78555f1e779116aa7587d853ea98308972be22f1
Pensionthieng Hrangchal <87723912+PesoHrangchal@users.noreply.github.com>
2022-09-29 14:30:06 +0530

    Add files via upload

---
+++ Adder.java
@@ -0,0 +1,6 @@
+//Hands-on Exercise 3
+public class Adder extends Arithmetic {
+	public static void main(String args[]) {
+		System.out.println(add(4,5));
+	}
+}
---
+++ Arithmetic.java
@@ -0,0 +1,6 @@
+//Hands-on Exercise 3
+public class Arithmetic {
+	static int add(int a,int b) {
+		return(a+b);
+	}
+}
