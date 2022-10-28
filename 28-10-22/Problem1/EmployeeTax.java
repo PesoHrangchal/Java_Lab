d17d3d8d4a794426e63fd0c9f8d484518f42b7a0
Pensionthieng Hrangchal <87723912+PesoHrangchal@users.noreply.github.com>
2022-10-28 13:15:23 +0530

    Create EmployeeTax.java

---
+++ 28-10-22/Problem1/EmployeeTax.java
@@ -0,0 +1,12 @@
+package lab;
+
+public class EmployeeTax {
+	public static void main(String args[]) {
+		TaxCalculator t=new TaxCalculator();
+		t.basicSalary=125000;
+		t.citizenship=true;
+		//t.calculateTax();
+		t.deductTax();
+		t.validateSalary();
+	}
+}
