283f3a558b0cee86a2b3858b4323d3cdbdaee8d5
Pensionthieng Hrangchal <87723912+PesoHrangchal@users.noreply.github.com>
2022-11-11 11:59:56 +0530

    Create Problem3.java
    Q1. Problem Statement 2(Updated Codes)

---
+++ 10-11-22/Problem3.java
@@ -0,0 +1,10 @@
+package lab;
+
+public class Problem3 {
+	public static void main(String...args) {
+		StringBuilder str=new StringBuilder("C://IBM//DB2//PROGRAM//DB2COPY1.EXE");
+		System.out.println("Drive:"+str.substring(0,3));
+		System.out.println("Folder:"+str.substring(3,6).concat("||")+str.substring(7,10).concat("||")+str.substring(11,18));
+		System.out.println("File:"+str.substring(20));
+	}
+}
