326aeda630cb6c8d572956f2d40f87e4bbd1fa1a
Pensionthieng Hrangchal <87723912+PesoHrangchal@users.noreply.github.com>
2022-12-02 12:43:45 +0530

    Update Q2_01.java

--- 01-12-22/Q2_01.java
+++ 01-12-22/Q2_01.java
@@ -9,35 +9,33 @@ import java.util.Scanner;
 
 public class Q2_01 {
 public static void main(String args[])throws IOException {
+	while(true) {
 	System.out.println("Choose an Option:");
 	System.out.println();
-	System.out.println("Press 1 to write to file...");
-	System.out.println("Press 2 to read from file...");
+	System.out.println("Press 1 to write to file:");
+	System.out.println("Press 2 to read from file:");
 	Scanner sc=new Scanner(System.in);
 	
 	int choice=sc.nextInt();
 	switch(choice) {
-		case 1:
-		{
+		case 1:try {
 			FileOutputStream f=new FileOutputStream("D:\\Test1.txt");//Destination
-			BufferedOutputStream b=new BufferedOutputStream(f);
 			System.out.println("Enter any text:");
-			String str=sc.nextLine();
-			byte b1[]=str.getBytes();
-			b.write(b1);
+			String s=sc.next();
+			byte b[]=s.getBytes();//converting string into byte array
+			f.write(b);
 			f.close();
-			b.close();
-			}
+			}catch(Exception e) {}
 			break;
-		case 2:
-			{FileInputStream f1=new FileInputStream("D:\\Test1.txt");//Destination
+		case 2:try{
+			FileInputStream f1=new FileInputStream("D:\\Test1.txt");//Destination
 			byte arr[]=f1.readAllBytes();
 			for(int i=0;i<arr.length;i++) {
 				System.out.print((char)arr[i]);
 			}
 			f1.close();
-			}
+			}catch(Exception e) {}
 			break;
 		default:
 			System.out.println("Enter a valid input!");
-}}}
+}}}}
