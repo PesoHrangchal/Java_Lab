35e4ebfeeb90f3f4a5dab216949b1720019cd045
Pensionthieng Hrangchal <ezepensionhmar@gmail.com>
2023-01-12 15:53:42 +0530

    Create BMS_Dao.java

---
+++ 12-01-23/BMS_Dao.java
@@ -0,0 +1,11 @@
+package com.BMS_Hibernate.dao;
+
+public interface BMS_Dao {
+	public void createAccount();
+	public void depositMoney();
+	public void withdrawMoney();
+	public void showAccount();
+	public void updateAccount();
+	public void deleteAccount();
+	
+}
